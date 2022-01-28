0a8d6ddc2daa0c33768934fd2a54561de04498a9
Jenniline <jennilineebai@gmail.com>
2022-01-28 18:18:44 +0100

    Added Hobbies and Occupation to the users profile in the users migration.

--- config/databassse.php
+++ config/databassse.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Str;
 
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $host = 'eu-cdbr-west-02.cleardb.net';
+    $username = 'b1e1416b73bd96';
+    $password = '';
+    $database = 'heroku_6eadb21256b8cb1';
+
 return [
 
     /*
@@ -46,16 +52,15 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => $host,
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
@@ -123,7 +128,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '').'_database'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
         ],
 
         'default' => [
