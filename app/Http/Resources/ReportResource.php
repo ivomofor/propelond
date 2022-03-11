46ad08f940ea20cde4a7c9fae7e060f825a9a711
Collins Tatang <makungongcollins@gmail.com>
2022-03-11 08:28:25 +0100

    Add report resource

---
+++ app/Http/Resources/ReportResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ReportResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return  [
+            'id' => $this->id,
+            'description' => $this->description,
+            'created_at' => (string) $this->created_at,
+            'updated_at' => (string) $this->updated_at,
+            'user_id' => User::find($this->user_id),
+            'post_id' => Post::find($this->post_id)
+        ];
+    }
+}
