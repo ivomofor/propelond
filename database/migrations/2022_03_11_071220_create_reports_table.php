5aaa5afc1c8e942db6e63aec105b2041548a8ad0
Collins Tatang <makungongcollins@gmail.com>
2022-03-11 08:27:30 +0100

    Add resource model and migration

---
+++ database/migrations/2022_03_11_071220_create_reports_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reports_post', function (Blueprint $table) {
+            $table->id();
+            $table->longText('description')->nullable();
+            $table->unsignedBigInteger('post_id');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reports');
+    }
+}
