f445b9d5c90437da69e520ec06496926f6740318
Collins Tatang <makungongcollins@gmail.com>
2022-03-13 04:02:53 +0100

    Add lost docs resource

---
+++ app/Http/Resources/LostDocumentResource.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class LostDocumentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        
+        return [
+            'id' => $this->id,
+            'fname' => $this->fname,
+            'lname' => $this->lname,
+            'dob' => $this->dob,
+            'profession' => $this->profession,
+            'doc_number' => $this->doc_number,
+            'email' => $this->email,
+            'phone_number' => $this->phone_number,
+            'country' => $this->country,
+            'city' => $this->city,
+            'image_path' => $this->image_path,
+            'created_at' => (string) $this->created_at,
+            'updated_at' => (string) $this->updated_at,
+            'user' => User::find($this->user_id)
+          ];
+    }
+}
